46c32e0a3c140a8f365ad64058366002560b4cfe
Jan Lützler <38535321+rosaLux161@users.noreply.github.com>
2022-12-10 14:26:47 +0100

    Update easy-arch.sh

--- easy-arch.sh
+++ easy-arch.sh
@@ -266,7 +266,7 @@ microcode_detector
 
 # Pacstrap (setting up a base sytem onto the new root).
 info_print "Installing the base system (it may take a while)."
-pacstrap /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers btrfs-progs rsync efibootmgr snapper reflector snap-pac zram-generator sudo lxde-gtk3 lxdm-gtk3 &>/dev/null
+pacstrap /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers btrfs-progs rsync efibootmgr snapper reflector snap-pac zram-generator sudo xfce4 lxdm-gtk3 &>/dev/null
 
 # Setting up the hostname.
 echo "$hostname" > /mnt/etc/hostname
