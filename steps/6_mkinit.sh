675a8d20b571b07a013f831d418ac631facba582
Jan Lützler <j.luetzler@reuter.de>
2022-12-20 12:46:14 +0100

    mkiit

--- steps/6_mkinit.sh
+++ steps/6_mkinit.sh
@@ -1,12 +1,3 @@
-DISK=/dev/nvme0n1
-locale="en_US.UTF-8"
-BTRFS="/dev/mapper/cryptroot"
-ESP="/dev/disk/by-partlabel/ESP"
-cryptroot="/dev/disk/by-partlabel/cryptroot"
-
-# Setting up keyboard layout.
-loadkeys de-latin1-nodeadkeys
-
 # Configuring /etc/mkinitcpio.conf.
 cat > /mnt/etc/mkinitcpio.conf <<EOF
 HOOKS=(base systemd autodetect keyboard sd-vconsole modconf kms block sd-encrypt filesystems fsck)
