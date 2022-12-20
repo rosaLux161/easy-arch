bfdb41f842acb9b90bcb7bdc6f09159f18a63a2a
Jan Lützler <j.luetzler@reuter.de>
2022-12-20 12:48:27 +0100

    chroot

--- steps/8_chroot_install.sh
+++ steps/8_chroot_install.sh
@@ -18,7 +18,7 @@ echo "title Arch Linux" >> /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-echo "options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard rw mem_sleep_default=deep >> /boot/loader/entries/arch.conf
+echo "options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard rw mem_sleep_default=deep" >> /boot/loader/entries/arch.conf
 
 touch /boot/loader/loader.conf
 echo "default arch" >> /boot/loader/loader.conf
