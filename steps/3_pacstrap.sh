821c74c5b2ad99d1a8d4c7dfae688710b4b4361c
Jan Lützler <j.luetzler@reuter.de>
2022-12-20 12:29:04 +0100

    pacstrap

--- steps/3_pacstrap.sh
+++ steps/3_pacstrap.sh
@@ -1,12 +1,3 @@
-DISK=/dev/nvme0n1
-locale="en_US.UTF-8"
-BTRFS="/dev/mapper/cryptroot"
-ESP="/dev/disk/by-partlabel/ESP"
-cryptroot="/dev/disk/by-partlabel/cryptroot"
-
-# Setting up keyboard layout.
-loadkeys de-latin1-nodeadkeys
-
 # Pacstrap (setting up a base sytem onto the new root).
 #pacman-key --refresh-keys
-pacstrap /mnt base linux intel-ucode base-devel linux-firmware linux-headers btrfs-progs rsync efibootmgr snapper reflector snap-pac zram-generator sudo xfce4 lxdm-gtk3 git &>/dev/null
+pacstrap /mnt base linux intel-ucode base-devel linux-firmware linux-headers btrfs-progs rsync efibootmgr snapper reflector snap-pac zram-generator sudo xfce4 lxdm-gtk3 git
